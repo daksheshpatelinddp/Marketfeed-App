0007cd7ef566127be24d2e395ea8c085ea98c67c
daksheshpatelinddp <daksheshpatelin@gmail.com>
2026-08-19 03:39:33 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,5 @@
-const C="marketfeed-v4";
-const A=[
-  "./",
-  "./index.html",
-  "./app.js",
-  "./style.css",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
-];
-
-self.addEventListener("install",e=>{
-  e.waitUntil(
-    caches.open(C)
-      .then(c=>c.addAll(A))
-      .then(()=>self.skipWaiting())
-  );
-});
-
-self.addEventListener("activate",e=>{
-  e.waitUntil(
-    caches.keys().then(keys=>
-      Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))
-    ).then(()=>self.clients.claim())
-  );
-});
-
-self.addEventListener("fetch",e=>{
-  if(e.request.method==="GET"){
-    e.respondWith(
-      caches.match(e.request).then(r=>r||fetch(e.request))
-    );
-  }
-});
+const C="marketfeed-v8";
+const A=["./","./index.html","./app.js?v=8","./style.css","./manifest.json","./icon-192.png","./icon-512.png"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener("fetch",e=>{if(e.request.method==="GET")e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
