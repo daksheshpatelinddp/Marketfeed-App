4244223a4ae5b2306b4ec052b8dee4b75e7b5a79
daksheshpatelinddp <daksheshpatelin@gmail.com>
2026-08-21 18:30:56 +0530

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,5 +0,0 @@
-const C="marketfeed-v9";
-const A=["./","./index.html","./app.js?v=9","./style.css","./manifest.json","./icon-192.png","./icon-512.png"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener("fetch",e=>{if(e.request.method==="GET")e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
